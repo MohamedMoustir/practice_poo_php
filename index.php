95687c2ecccf1057f7452da8cd0f81f9dd65d953
MohamedMoustir <itsmoustir@gmail.com>
2024-12-24 17:20:58 +0100

    Fin.

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo $users->getNom();
 }
 
+
 $query = $systeme->link->query('SELECT * FROM utilisateurs WHERE type_utilisateur = "Médecin"');
 
 $Médecins = [];
